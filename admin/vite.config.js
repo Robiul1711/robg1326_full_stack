897e90edd20b83374511a30fe7e01f0b823f999c
Muhammad Sefat <muhammadsefat55@gmail.com>
2026-09-24 12:47:57 +0600

    Update admin package configuration

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -4,8 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    port: 5173,
-    host: true,
-  },
+  // server: {
+  //   port: 5173,
+  //   host: true,
+  // },
 });
